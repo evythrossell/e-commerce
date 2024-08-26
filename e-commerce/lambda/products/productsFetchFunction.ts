2fd19de3ea03039c3f7bb708be32acc244ac522a
EvyOliveira <evelynoliveira.eo@gmail.com>
2024-08-25 21:32:43 -0300

    Add lambda function log based on lambda and API gateway request id

--- e-commerce/lambda/products/productsFetchFunction.ts
+++ e-commerce/lambda/products/productsFetchFunction.ts
@@ -3,7 +3,11 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda
 export async function handler(event: APIGatewayProxyEvent,
     context: Context) : Promise<APIGatewayProxyResult> {
 
+        const lambdaRequestId = context.awsRequestId
+        const apiRequestId = event.requestContext.requestId
         const method = event.httpMethod
+
+        console.log(`API Gateway requestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`)
         if (event.resource === "/products") {
             if (method === "GET") {
                 console.log('GET method for products')
