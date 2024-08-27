c77d3f7e6a0a8c26775fcf84f06869a97bfae921
EvyOliveira <evelynoliveira.eo@gmail.com>
2024-08-26 22:33:50 -0300

    Feat API gateway stack

---
+++ e-commerce/lib/e-commerceApi-stack.ts
@@ -0,0 +1,18 @@
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
+import * as apigateway from "aws-cdk-lib/aws-apigateway"
+import * as cwlogs from "aws-cdk-lib/aws-logs"
+import * as cdk from "aws-cdk-lib"
+
+import { Construct } from "constructs"
+
+export class EcommerceApiStack extends cdk.Stack {
+
+    constructor(scope: Construct, id : string, props?: cdk.StackProps) {
+        super(scope, id, props)
+
+        const api = new apigateway.RestApi(this, "EcommerceApi", {
+            restApiName: "EcommerceApi",
+        })
+    }
+}
