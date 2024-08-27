ce1daeb78b287d3d3b549403de54c59a7a7be76d
EvyOliveira <evelynoliveira.eo@gmail.com>
2024-08-26 22:50:04 -0300

    Feat api gateway integration with product search lambda funtion

--- e-commerce/lib/e-commerceApi-stack.ts
+++ e-commerce/lib/e-commerceApi-stack.ts
@@ -6,13 +6,21 @@ import * as cdk from "aws-cdk-lib"
 
 import { Construct } from "constructs"
 
+interface EcommerceApiStackProps extends cdk.StackProps {
+    productsFetchHandler: lambdaNodeJS.NodejsFunction
+}
+
 export class EcommerceApiStack extends cdk.Stack {
 
-    constructor(scope: Construct, id : string, props?: cdk.StackProps) {
+    constructor(scope: Construct, id : string, props: EcommerceApiStackProps) {
         super(scope, id, props)
 
         const api = new apigateway.RestApi(this, "EcommerceApi", {
             restApiName: "EcommerceApi",
         })
+
+        const productFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
+        const productsResource = api.root.addResource("products")
+        productsResource.addMethod("GET", productFetchIntegration)
     }
 }
