92a382877ee507af3c3aeafab526b5e3f76a2ff9
EvyOliveira <evelynoliveira.eo@gmail.com>
2024-09-03 20:28:47 -0300

    Feat ProductsAdmin lambda function

--- e-commerce/lib/productsApp-stack.ts
+++ e-commerce/lib/productsApp-stack.ts
@@ -8,6 +8,7 @@ import { Construct } from "constructs"
 export class ProductAppStack extends cdk.Stack {
     readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
     readonly productsDb: dynamodb.Table
+    readonly productsAdminHandler: lambdaNodeJS.NodejsFunction
 
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props)
@@ -23,22 +24,40 @@ export class ProductAppStack extends cdk.Stack {
             readCapacity: 1,
             writeCapacity: 1
         })
-        this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, 
+        this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this,
             "ProductsFetchFunction", {
-                functionName: "ProductsFetchFunction",
-                entry: "lambda/products/productsFetchFunction.ts",
-                handler: "handler",
-                runtime: lambda.Runtime.NODEJS_20_X,
-                memorySize: 512,
-                timeout: cdk.Duration.seconds(5),
-                bundling: {
-                    minify: true,
-                    sourceMap: false
-                },
-                environment: {
-                    PRODUCTS_DB: this.productsDb.tableName
-                }
-            })
-            this.productsDb.grantReadData(this.productsFetchHandler)
+            functionName: "ProductsFetchFunction",
+            entry: "lambda/products/productsFetchFunction.ts",
+            handler: "handler",
+            runtime: lambda.Runtime.NODEJS_20_X,
+            memorySize: 512,
+            timeout: cdk.Duration.seconds(5),
+            bundling: {
+                minify: true,
+                sourceMap: false
+            },
+            environment: {
+                PRODUCTS_DB: this.productsDb.tableName
+            }
+        })
+        this.productsDb.grantReadData(this.productsFetchHandler)
+
+        this.productsAdminHandler = new lambdaNodeJS.NodejsFunction(this,
+            "ProductsAdminFunction", {
+            functionName: "ProductsAdminFunction",
+            entry: "lambda/products/ProductsAdminFunction.ts",
+            handler: "handler",
+            runtime: lambda.Runtime.NODEJS_20_X,
+            memorySize: 512,
+            timeout: cdk.Duration.seconds(5),
+            bundling: {
+                minify: true,
+                sourceMap: false
+            },
+            environment: {
+                PRODUCTS_DB: this.productsDb.tableName
+            }
+        })
+        this.productsDb.grantWriteData(this.productsAdminHandler)
     }
 }
